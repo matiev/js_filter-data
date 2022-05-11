58aceaeb6cd5a1a0fb494a682b507eebc07f3a12
matiev <matiev@live.com>
2022-05-11 18:38:51 +0300

    first

---
+++ app.js
@@ -0,0 +1,28 @@
+let filterLinks = document.querySelectorAll('.filter__link'),
+    contentItems = document.querySelectorAll('.content__item'),
+    content = document.querySelector('.content');
+    const items = [...content.children];
+
+filterLinks.forEach(item => {
+    content.innerHTML = '';    
+    item.addEventListener('click', event => {    
+        event.preventDefault();
+        filterLinks.forEach(item => item.classList.remove('active'));
+        if (!item.closest('.active')) {
+            item.classList.add('active');
+
+            let iLinkData = item.getAttribute('data-link');
+            let season = content.querySelectorAll(`[data-s="${iLinkData}"]`);
+
+            content.innerHTML = '';
+
+            items.forEach(item => {
+                if (item.getAttribute('data-s') === iLinkData) {
+                    content.append(item)
+                }
+            })
+        }
+    });
+});
+
+filterLinks[0].click();
